3b55e3190ffbb10d8dc6af27222445ccc6359b90
vetal <semeniv09@ukr.net>
2025-08-19 19:50:44 +0000

    create file for backup-task.sh

--- task.sh
+++ task.sh
@@ -1,1 +1,13 @@
-#! /bin/bash
+#!/bin/bash
+
+
+mysqldump -u backup -pBackupPass123! ShopDB > ShopDBReserve.sql
+
+
+mysql -u backup -pBackupPass123! ShopDBReserve < ShopDBReserve.sql
+
+
+mysqldump -u backup -pBackupPass123! --no-create-info ShopDB > ShopDBDevelopment.sql
+
+
+mysql -u backup -pBackupPass123! ShopDBDevelopment < ShopDBDevelopment.sql
